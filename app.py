ef1c71acac2f29e4b2462f4239ff532e0bcbbad9
Matheus Caetano <caetanomatheus142@gmail.com>
2026-05-05 07:35:31 -0400

    criado template de cadastro de dados para implementação nos parametros de busca e alterada forma de componentização, chamando a rota na main e centralizando os componentes.

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from flask import app, jsonify, request, render_template
+from flask import Flask
 from services.collect import get_editais
 from services.parser import organize
 from services.filter import filter
@@ -5,24 +7,31 @@ from services.storage import save, exists
 from services.email_service import send_email
 from services.collect_2 import coletar_licitacoes_reais, enriquecer_com_itens
 
-def run():
-    editais = coletar_licitacoes_reais()
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/licitacoes", methods=["POST"])
+def coletar_licitacoes():
+    data = request.get_json()
+    max_paginas = data.get("max_paginas", 5)
+    tamanho = data.get("tamanho", 10)
+    palavras_chave = data.get("palavras_chave")
+    number_days = data.get("number_days", 7)
+
+    print("Dados recebidos:", data)
+
+    editais = coletar_licitacoes_reais(max_paginas=max_paginas, tamanho=tamanho, palavras_chave=palavras_chave, number_days=number_days)
+    # print(f"Editais coletados: {len(editais)}")
+    # print("editais: ", editais)
+
     editais_com_itens = enriquecer_com_itens(editais)
     dados = organize(editais_com_itens)
+
+    send_email(dados)
+    return jsonify(dados)
     
-
-    password = ["software juridico", "saas juridico", "recorte", "acompanhamento processos","DEDETIZAÇÃO", "Limpeza"]
-
-    filtered = filter(dados,password )
-    print(filtered)
-    news = []
-
-    for item in filtered:
-        if not exists(item["id"]):
-            save(item)
-            news.append(item)
-
-    send_email(filtered) #depois dos testes, trocar para news
-
 if __name__ == "__main__":
-    run()
+    app.run(debug=True)
